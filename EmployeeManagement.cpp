cd6bb72740005557d164937b8d122363b92fb066
Parth Chauhan <parthchauhan25125@gmail.com>
2026-07-08 19:15:43 +0530

    Planning EmployeeMG

---
+++ EmployeeManagement.cpp
@@ -0,0 +1,19 @@
+/*
+    Devloper:Parth Chauhan
+    DOC:08-07-2026
+    Subject:Learning Structure
+
+
+    
+    Planning:
+        Sub:Employee Management System using Structure
+        
+        structure contains:
+            empID,empName,yearsOfExp,salary
+        Input:
+            User has to give empID,empName,yearsOfExp,salary
+        Conditions:
+            If the years of Experience is greater then 5,increase current salary with +5000,
+            If the years of Experience is greater then 10,increase current salary with +10000
+
+*/
