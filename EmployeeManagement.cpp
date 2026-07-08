78fb4df44aff76444578be903c2884df58c5615e
Parth Chauhan <parthchauhan25125@gmail.com>
2026-07-08 21:57:11 +0530

    Refactor Employee Management System: Update planning section and enhance salary increment logic; add validation and display functions

--- EmployeeManagement.cpp
+++ EmployeeManagement.cpp
@@ -4,7 +4,7 @@
     Subject:Learning Structure
     
     Planning:
-        Sub:Employee Management System using Structure
+        About:Employee Management System using Structure
         
         structure contains:
             empID,empName,yearsOfExp,salary
@@ -18,7 +18,9 @@
 using namespace std;
 #define MAX_EMP_SIZE 5
 #define MAX_EMP_NAME_SIZE 10
-//prototype
+#define YOE_INCREMENT_5000 5
+#define YOE_INCREMENT_10000 10
+
 struct Employee{
     int empId;
     char empName[MAX_EMP_NAME_SIZE];
@@ -26,13 +28,50 @@ struct Employee{
     float salary;
 }e[MAX_EMP_SIZE];
 
-int display(Employee* obj);
+//prototype
+int validateNo(int n);
+int setData(int n,Employee* ep);
+int display(int n,Employee* obj);
 
-int display(Employee* obj){
-    cout<<"EMPLOYEE ID:"<<obj->empId<<endl;
-    cout<<"EMPLOYEE NAME:"<<obj->empName<<endl;
-    cout<<"EMPLOYEE YOE:"<<obj->yearsOfExp<<endl;
-    cout<<"EMPLOYEE Salary:"<<obj->salary<<endl;
+//Functions with body
+
+//function:validateNo check the no with MAX_EMP_SIZE if it lesser or equal then it will allow else program close
+int validateNo(int n){
+    if(n>MAX_EMP_SIZE){
+        cout<<"only "<<MAX_EMP_SIZE<<" can added at a time"<<endl;
+        exit(0);
+    }
+    return 1;
+}
+
+//function:setData set the data to the object of the struct
+int setData(int n,Employee* ep){
+    for(int i=0;i<n;i++){   
+        cout<<"Enter Employee ID :";
+        cin>>(ep+i)->empId;
+        cout<<"Enter Employee Name :";
+        cin>>(ep+i)->empName;
+        cout<<"Enter Employee Years Of Experince :";
+        cin>>(ep+i)->yearsOfExp;
+        cout<<"Enter Employee Salary :";
+        cin>>(ep+i)->salary;
+        if((ep+i)->yearsOfExp > YOE_INCREMENT_10000){
+            (ep+i)->salary+=10000;
+        }else if((ep+i)->yearsOfExp > YOE_INCREMENT_5000){
+            (ep+i)->salary+=5000;
+        }
+}
+    return 0;
+}
+
+int display(int n,Employee* obj){
+    for(int i=0;i<n;i++)
+    {
+        cout<<"EMPLOYEE ID:"<<(obj+i)->empId<<endl;
+        cout<<"EMPLOYEE NAME:"<<(obj+i)->empName<<endl;
+        cout<<"EMPLOYEE YOE:"<<(obj+i)->yearsOfExp<<endl;
+        cout<<"EMPLOYEE Salary:"<<(obj+i)->salary<<endl<<endl;
+    }
     return 0;
 }
 
@@ -42,26 +81,7 @@ int main(){
 
     cout<<"Enter Number Of Employee You Wants to Add ";
     cin>>n;
-    if(n>MAX_EMP_SIZE){
-        cout<<"only "<<MAX_EMP_SIZE<<" can added at a time"<<endl;
-        cout<<"Enter Number Of Employee You Wants to Add ";
-        cin>>n;
-    }
-    for(int i=0;i<n;i++){
-        cout<<"Enter Employee ID :";
-        cin>>(ep+i)->empId;
-        cout<<"Enter Employee Name :";
-        cin>>(ep+i)->empName;
-        cout<<"Enter Employee Years Of Experince :";
-        cin>>(ep+i)->empName;
-        cout<<"Enter Employee Salary :";
-        cin>>(ep+i)->salary;
-    }
-
-    cout<<"Displaying Employee Records :";
-    
-    for(int i=0;i<n;i++){
-        display((ep+i));
-    }
-
+    validateNo(n);
+    setData(n,ep);
+    display(n,ep);
 }
